679827f7a15a4e3f1f75c69a5764ca0da547fa1f
Alexander <hello@alexander.works>
2020-04-05 16:27:11 +0300

    Using Proxy v2

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const request = require('request-promise').defaults({
   // Full of a proxy: http://username:password@ip:port
-  proxy: 'http://103.123.246.54:8080',
+
+  // proxy: 'http://103.123.246.54:8080',
+  proxy: 'http://31.146.31.182:3128',
 });
 
 (async () => {
