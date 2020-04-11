92863cd84afc57c3daf87d25830ba9965fbdd7de
Alexander <hello@alexander.works>
2020-04-11 19:10:42 +0300

    Puppeteer: Login into Pinterest and browsing through account

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -6,17 +7,37 @@ const puppeteer = require('puppeteer');
   });
   const page = await browser.newPage();
 
-  await page.setRequestInterception(true);
+  // Login into Pinterest account
+  await page.goto('https://pinterest.com');
+  await page.waitFor('div[data-test-id="simple-login-button"] > button');
+  await page.click('div[data-test-id="simple-login-button"] > button');
 
-  page.on('request', (request) => {
-    if (['images', 'stylesheet', 'font'].includes(request.resourceType())) {
-      request.abort();
-    } else {
-      request.continue();
-    }
+  await page.waitFor(500);
+  // await page.waitForNavigation();
+
+  await page.waitFor('input[name="id"]');
+  await page.type('input[name="id"]', 'username@email.com', {
+    delay: 100,
   });
+  await page.type('input[name="password"]', 'userPassword123', {
+    delay: 100,
+  });
+  await page.click('button[type="submit"]');
 
-  await page.goto('https://amazon.com');
+  // Go to your profile page
+
+  await page.waitFor('div[aria-label="Your profile"] > a');
+  await page.setViewport({ width: 1200, height: 1200 });
+  await page.click('div[aria-label="Your profile"] > a');
+  await page.waitFor('a[href=/username/web-app-design/"]');
+  await page.click('a[href=/username/web-app-design/"]');
+
+  // await page.waitFor(500);
+  // await page.goto('/username/web-app-design/');
+
+  // Scraping images
+  const IMAGE_SELECTOR = '';
+
   debugger;
   // await browser.close();
 })();
