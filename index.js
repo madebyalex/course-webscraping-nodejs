172cbaab358770f1b0393a7a31ca769ecdb79374
Alexander <hello@alexander.works>
2020-04-11 19:16:35 +0300

    Puppeteer: Basic HTML authentification

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const puppeteer = require('puppeteer');
-const fs = require('fs');
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -7,36 +6,9 @@ const fs = require('fs');
   });
   const page = await browser.newPage();
 
-  // Login into Pinterest account
-  await page.goto('https://pinterest.com');
-  await page.waitFor('div[data-test-id="simple-login-button"] > button');
-  await page.click('div[data-test-id="simple-login-button"] > button');
+  await page.authenticate({ username: 'admin', password: '123456' });
 
-  await page.waitFor(500);
-  // await page.waitForNavigation();
-
-  await page.waitFor('input[name="id"]');
-  await page.type('input[name="id"]', 'username@email.com', {
-    delay: 100,
-  });
-  await page.type('input[name="password"]', 'userPassword123', {
-    delay: 100,
-  });
-  await page.click('button[type="submit"]');
-
-  // Go to your profile page
-
-  await page.waitFor('div[aria-label="Your profile"] > a');
-  await page.setViewport({ width: 1200, height: 1200 });
-  await page.click('div[aria-label="Your profile"] > a');
-  await page.waitFor('a[href=/username/web-app-design/"]');
-  await page.click('a[href=/username/web-app-design/"]');
-
-  // await page.waitFor(500);
-  // await page.goto('/username/web-app-design/');
-
-  // Scraping images
-  const IMAGE_SELECTOR = '';
+  await page.goto('https://httpbin.org/basic-auth/admin/123456');
 
   debugger;
   // await browser.close();
