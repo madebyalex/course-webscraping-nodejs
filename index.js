e8cfe5677eec6ab7f884c80e2266e031e02f0d22
Alexander <hello@alexander.works>
2020-04-11 17:05:41 +0300

    Puppeteer: Making Websites Load Much Faster with rejecting to load unnecessary resources like images, stylesheets and fonts

--- index.js
+++ index.js
@@ -5,17 +5,18 @@ const puppeteer = require('puppeteer');
     headless: false,
   });
   const page = await browser.newPage();
-  await page.goto('https://instagram.com');
-  // await page.waitFor('a[href="/accounts/login/?source=auth_switcher"');
-  // await page.click('a[href="/accounts/login/?source=auth_switcher"');
 
-  await page.waitFor(500);
+  await page.setRequestInterception(true);
 
-  await page.waitFor('input[name="username"]');
-  await page.type('input[name="username"]', 'cool-user');
-  await page.type('input[name="password"]', 'strongPassword');
-  await page.click('button[type="submit"]');
+  page.on('request', (request) => {
+    if (['images', 'stylesheet', 'font'].includes(request.resourceType())) {
+      request.abort();
+    } else {
+      request.continue();
+    }
+  });
 
+  await page.goto('https://amazon.com');
   debugger;
   // await browser.close();
 })();
